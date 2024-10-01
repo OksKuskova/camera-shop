9866b7d49e78f51ff9c58b9f083a837e883ab933
OksKuskova <oskanchik.kuskova@gmail.com>
2024-10-01 11:54:59 +0200

    Обновит локаль в dayjs, реализует вывод даты отзыва в надлежащем формате

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,29 @@
 import dayjs from 'dayjs';
+import updateLocale from 'dayjs/plugin/updateLocale';
+import { DateFormat } from './const';
 
-export const humanizeDate = (date: string, format: string) => dayjs(date).format(format);
+dayjs.extend(updateLocale);
+
+dayjs.updateLocale('en', {
+  months: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']
+});
+
+const addCorrectMonthEnding = (word: string) => {
+  if (word.endsWith('ь') || word.endsWith('й')) {
+    return `${word.slice(0, -1)}я`;
+  }
+  return `${word}а`;
+};
+
+export const humanizeDate = (date: string, format: string) => {
+  const humanizedDate = dayjs(date).format(format);
+
+  if (format === DateFormat.DayMonth) {
+    const month = humanizedDate.split(' ')[1];
+    const monthWithCorrectEnding = addCorrectMonthEnding(month.toLowerCase());
+
+    return humanizedDate.replace(month, monthWithCorrectEnding);
+  }
+
+  return humanizedDate;
+};
